d08121d601e8de90bf6c1698f9990d0307516dea
kevin caldwell <kvn.caldwll@gmail.com>
2016-07-19 00:14:02 -0700

    play, pause, stop, scrub audio file

--- Player Player/Player Player/ViewController.swift
+++ Player Player/Player Player/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func scrub(sender: AnyObject) {
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+    }
+    @IBOutlet weak var scrubSlider: UISlider!
+    
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        player.pause()
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("good-life", ofType: "mp3")!))
+        } catch {
+            // throw error message in case
+        }
+    }
+    
+    func updateScrubSlider() {
+        scrubSlider.value = Float(player.currentTime)
+    }
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("good-life", ofType: "mp3")!))
+            scrubSlider.maximumValue = Float(player.duration)
+        } catch {
+            // throw error message in case
+        }
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.updateScrubSlider), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
